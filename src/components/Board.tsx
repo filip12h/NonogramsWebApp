1e62a661636ee761c73217cfe08f10858b315139
filip12h <martello2311@gmail.com>
2021-03-29 01:14:43 +0200

    Pridané stláčanie buniek
    Konečne sa toto podarilo sprevádzkovať, zatiaľ len dvojfarebne.

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -1,4 +1,29 @@
-import * as React from 'react';
+import React, { useState } from 'react';
+
+type TileProps = {
+    id: number;
+    col5: boolean;
+};
+
+const Tile: React.FC<TileProps> = ({ id, col5 }) => {
+    const [clicked, changeClicked] = useState(false);
+
+    const clickCell = () => {
+        changeClicked(!clicked);
+    };
+
+    return (
+        <td key={id.toString()}>
+            <button
+                type="button"
+                onClick={clickCell}
+                className={(col5 ? 'column5' : '') + (clicked ? ' clicked' : '')}
+            >
+                {null}
+            </button>
+        </td>
+    );
+};
 
 type BoardProps = {
     width: number;
@@ -10,9 +35,9 @@ const Board: React.FC<BoardProps> = (props) => {
         const elements = [];
         for (let i = 0; i < props.width; i += 1) {
             if (i % 5 === 0) {
-                elements.push(<td className="column5" key={i.toString()} />);
+                elements.push(<Tile id={i} col5 />);
             } else {
-                elements.push(<td key={i.toString()} />);
+                elements.push(<Tile id={i} col5={false} />);
             }
         }
         return elements;
