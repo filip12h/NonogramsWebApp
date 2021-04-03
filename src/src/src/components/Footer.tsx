f8f12a2f3b60de6e35f8217418bc9ac342f0ab09
filip12h <martello2311@gmail.com>
2021-04-03 18:53:49 +0200

    uper left corner merged
    colspan, rowspan... to fulfill this task, I had to make couple of more functions and it looks prettier as a whole

---
+++ src/src/src/components/Footer.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Footer: React.FC = () => {
+    return (
+        <>
+            <footer>
+                copy Filip Husár
+                <br />
+                FMFI UK
+                <br />
+                2021
+            </footer>
+        </>
+    );
+};
+
+export default Footer;
