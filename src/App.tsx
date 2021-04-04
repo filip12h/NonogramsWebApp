f8cecdbf0c3059103dacca3fb1e3ef20ec75f787
filip12h <martello2311@gmail.com>
2021-04-04 23:42:55 +0200

    isSolvedController, crossOut Cell
    ...but I do not like that every 5th column has bad right side of cells

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,129 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import Footer from './components/Footer';
 import Board from './components/Board';
 import Top from './components/Top';
 
 const App: React.FC = () => {
+    const solution = [
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 1, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 1, 1, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 1, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 1, 1, 0, 0],
+
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 1, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 1, 0, 0],
+        [0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 0],
+        [0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0],
+        [0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 1, 1, 1, 0, 1],
+
+        [0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1],
+        [0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 1, 1, 1],
+        [0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 1, 0, 1, 0, 0, 0, 0, 1, 0, 0, 1, 1, 1, 1, 0],
+        [0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0],
+        [0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 1, 1, 0, 1, 1, 0, 0, 0, 0],
+
+        [1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0],
+        [1, 1, 0, 1, 1, 0, 0, 0, 1, 1, 1, 1, 0, 1, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 1, 1, 1, 1, 1, 1, 0, 0, 0, 1, 1, 1, 1, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0],
+    ];
+    const width = solution[0].length;
+    const height = solution.length;
+
+    // felt cute, might delete later
+    const solvingProgress = [[]];
+
+    // we define numbers bottom-up
+    const outerUpperNumbers = [
+        [2],
+        [4],
+        [1, 5],
+        [4, 3],
+        [2, 2],
+        [2, 1],
+        [2, 2],
+        [2, 2],
+        [1, 1, 1],
+        [2, 1, 1, 1],
+        [1, 5, 1],
+        [1, 3, 1],
+        [3, 4, 1],
+        [4, 1],
+        [2, 1],
+        [1, 1, 1],
+        [2, 3, 2, 4],
+        [8, 2, 7],
+        [2, 1, 7, 1],
+        [2, 1, 5, 1],
+        [3, 4, 2],
+        [6, 3],
+        [9],
+        [3, 1],
+        [3],
+    ];
+
+    // we define numbers left to right
+    const outerLeftNumbers = [
+        [3],
+        [2, 1],
+        [3, 2],
+        [3, 1],
+        [4, 2],
+        [3, 1],
+        [4, 1],
+        [8, 5],
+        [4, 10],
+        [2, 2, 3, 1],
+        [1, 4],
+        [2, 1, 4],
+        [1, 2, 1, 1, 4],
+        [2, 1, 1, 1, 1],
+        [3, 1, 1, 2, 2],
+        [4, 4, 4],
+        [2, 2, 4, 1, 3],
+        [6, 4, 2],
+        [5, 7],
+        [4, 2],
+    ];
+
+    // in the beginning all empty cells are considered as correct, so we just count white cells
+    const numOfCorrectTiles = solution.reduce(
+        (a, b) => a + b.reduce((c, d) => (d === 0 ? c + 1 : c), 0),
+        0,
+    );
+
+    const [correct, change] = useState(numOfCorrectTiles);
+
+    const checkCorrectness = (id: number, clicked: number) => {
+        // we increase/decrease the number of correct cells whether the cell is in solution or not
+        if (!(solution[Math.floor(id / width)][id % width] - clicked === 0)) {
+            if (correct + 1 === width * height) alert('CONGRATULATIONS!\nNONOGRAM IS DONE');
+            change(correct + 1);
+        } else {
+            if (correct - 1 === width * height) alert('CONGRATULATIONS!\nNONOGRAM IS DONE');
+            change(correct - 1);
+        }
+        // for some reason next line alerted too late
+        // if (correct === width * height) alert('CONGRATULATIONS!\nNONOGRAM IS DONE');
+    };
+
+    // const [correct, change] = useState(numOfCorrectTiles);
     return (
         <>
             <Top />
-            <Board width={30} height={25} />
+            <div>{correct}</div>
+            <Board
+                width={solution[0].length}
+                height={solution.length}
+                leftNum={outerLeftNumbers}
+                upNum={outerUpperNumbers}
+                solution={solution}
+                progress={solvingProgress}
+                checkCorrect={checkCorrectness}
+            />
             <Footer />
         </>
     );
--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -3,13 +3,32 @@ import React, { useState } from 'react';
 type TileProps = {
     id: number;
     col5: boolean;
+    checkCorrect: (a: number, b: number) => void;
 };
 
-const Tile: React.FC<TileProps> = ({ id, col5 }) => {
-    const [clicked, changeClicked] = useState(false);
+const Tile: React.FC<TileProps> = ({ id, col5, checkCorrect }) => {
+    const [clicked, changeClicked] = useState(0);
+    const [crossed, changeCross] = useState(false);
 
     const clickCell = () => {
-        changeClicked(!clicked);
+        changeCross(false);
+        changeClicked(1 - clicked);
+        checkCorrect(id, clicked);
+        /*
+        if (clicked === inSolution) {
+            change(correct + 1);
+        } else {
+            change(correct - 1);
+        }
+        */
+    };
+
+    // afraid I do not know how to change that any so far...
+    const toCross = (e: any) => {
+        e.preventDefault();
+        if (!clicked) {
+            changeCross(!crossed);
+        }
     };
 
     return (
@@ -17,7 +36,12 @@ const Tile: React.FC<TileProps> = ({ id, col5 }) => {
             <button
                 type="button"
                 onClick={clickCell}
-                className={(col5 ? 'column5' : '') + (clicked ? ' clicked' : '')}
+                className={
+                    (col5 ? 'column5' : '') +
+                    (clicked ? ' clicked' : '') +
+                    (crossed ? ' crossed' : '')
+                }
+                onContextMenu={toCross}
             >
                 {null}
             </button>
@@ -25,108 +49,31 @@ const Tile: React.FC<TileProps> = ({ id, col5 }) => {
     );
 };
 
-const solution = [
-    [1, 0, 0, 1, 1, 1, 0, 1, 0, 0, 1, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 1, 0, 0, 0, 1, 0, 1, 1, 0],
-    [1, 0, 0, 1, 1, 1, 0, 1, 0, 0, 1, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 1, 0, 0, 0, 1, 0, 1, 1, 0],
-    [1, 0, 0, 1, 1, 1, 0, 1, 0, 0, 1, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 1, 0, 0, 0, 1, 0, 1, 1, 0],
-    [1, 0, 0, 1, 1, 1, 0, 1, 0, 0, 1, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 1, 0, 0, 0, 1, 0, 1, 1, 0],
-    [1, 0, 0, 1, 1, 1, 0, 1, 0, 0, 1, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 1, 0, 0, 0, 1, 0, 1, 1, 0],
-    [1, 0, 0, 1, 1, 1, 0, 1, 0, 0, 1, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 1, 0, 0, 0, 1, 0, 1, 1, 0],
-    [1, 0, 0, 1, 1, 1, 0, 1, 0, 0, 1, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 1, 0, 0, 0, 1, 0, 1, 1, 0],
-    [1, 0, 0, 1, 1, 1, 0, 1, 0, 0, 1, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 1, 0, 0, 0, 1, 0, 1, 1, 0],
-    [1, 0, 0, 1, 1, 1, 0, 1, 0, 0, 1, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 1, 0, 0, 0, 1, 0, 1, 1, 0],
-    [1, 0, 0, 1, 1, 1, 0, 1, 0, 0, 1, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 1, 0, 0, 0, 1, 0, 1, 1, 0],
-    [1, 0, 0, 1, 1, 1, 0, 1, 0, 0, 1, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 1, 0, 0, 0, 1, 0, 1, 1, 0],
-    [1, 0, 0, 1, 1, 1, 0, 1, 0, 0, 1, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 1, 0, 0, 0, 1, 0, 1, 1, 0],
-    [1, 0, 0, 1, 1, 1, 0, 1, 0, 0, 1, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 1, 0, 0, 0, 1, 0, 1, 1, 0],
-    [1, 0, 0, 1, 1, 1, 0, 1, 0, 0, 1, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 1, 0, 0, 0, 1, 0, 1, 1, 0],
-    [1, 0, 0, 1, 1, 1, 0, 1, 0, 0, 1, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 1, 0, 0, 0, 1, 0, 1, 1, 0],
-    [1, 0, 0, 1, 1, 1, 0, 1, 0, 0, 1, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 1, 0, 0, 0, 1, 0, 1, 1, 0],
-    [1, 0, 0, 1, 1, 1, 0, 1, 0, 0, 1, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 1, 0, 0, 0, 1, 0, 1, 1, 0],
-    [1, 0, 0, 1, 1, 1, 0, 1, 0, 0, 1, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 1, 0, 0, 0, 1, 0, 1, 1, 0],
-    [1, 0, 0, 1, 1, 1, 0, 1, 0, 0, 1, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 1, 0, 0, 0, 1, 0, 1, 1, 0],
-    [1, 0, 0, 1, 1, 1, 0, 1, 0, 0, 1, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 1, 0, 0, 0, 1, 0, 1, 1, 0],
-    [1, 0, 0, 1, 1, 1, 0, 1, 0, 0, 1, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 1, 0, 0, 0, 1, 0, 1, 1, 0],
-    [1, 0, 0, 1, 1, 1, 0, 1, 0, 0, 1, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 1, 0, 0, 0, 1, 0, 1, 1, 0],
-    [1, 0, 0, 1, 1, 1, 0, 1, 0, 0, 1, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 1, 0, 0, 0, 1, 0, 1, 1, 0],
-    [1, 0, 0, 1, 1, 1, 0, 1, 0, 0, 1, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 1, 0, 0, 0, 1, 0, 1, 1, 0],
-    [1, 0, 0, 1, 1, 1, 0, 1, 0, 0, 1, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 1, 0, 0, 0, 1, 0, 1, 1, 0],
-];
-
-const solvingProgress = [];
-
-const outerUpperNumbers = [
-    [4, 7, 5],
-    [3],
-    [5],
-    [1, 2],
-    [6],
-    [4, 7],
-    [3],
-    [5],
-    [1, 2],
-    [6],
-    [4, 7],
-    [3],
-    [5],
-    [1, 2],
-    [6],
-    [4, 7],
-    [3],
-    [5],
-    [1, 2],
-    [6],
-    [4, 7],
-    [3],
-    [5],
-    [1, 2],
-    [6],
-    [4, 7],
-    [3],
-    [5],
-    [1, 2],
-    [6],
-];
-
-const outerLeftNumbers = [
-    [5],
-    [1, 2],
-    [6],
-    [4, 7],
-    [3],
-    [5],
-    [1, 2],
-    [6],
-    [4, 7, 1],
-    [3],
-    [5],
-    [1, 2],
-    [6],
-    [4, 7],
-    [3],
-    [5],
-    [1, 2],
-    [6],
-    [4, 7],
-    [3],
-    [5],
-    [1, 2],
-    [6],
-    [4, 7],
-    [3],
-];
-
-/*   TODO: we need to calculate this automatically   */
-const numOfUpperRows = Math.max(...outerUpperNumbers.map((col) => col.length));
-const numOfLeftColumns = Math.max(...outerLeftNumbers.map((row) => row.length));
-
 type BoardProps = {
     width: number;
     height: number;
+    solution: number[][];
+    progress: number[][];
+    upNum: number[][];
+    leftNum: number[][];
+    checkCorrect: (a: number, b: number) => void;
 };
+const Board: React.FC<BoardProps> = ({
+    width,
+    height,
+    solution,
+    progress,
+    upNum,
+    leftNum,
+    checkCorrect,
+}) => {
+    // numberOfTiles = props.width * props.height;
 
-const Board: React.FC<BoardProps> = (props) => {
-    if (solution.length === 5 && solvingProgress.length === 1) {
+    /*   TODO: we need to calculate this automatically   */
+    const numOfUpperRows = Math.max(...upNum.map((col) => col.length));
+    const numOfLeftColumns = Math.max(...leftNum.map((row) => row.length));
+
+    if (solution.length === 5 && progress.length === 1) {
         // eslint-disable-next-line
         alert('haha');
     }
@@ -134,12 +81,12 @@ const Board: React.FC<BoardProps> = (props) => {
     const outerLeftNumbersGenerator = (i: number) => {
         const elements = [];
         for (let j = 0; j < numOfLeftColumns; j += 1) {
-            if (outerLeftNumbers[i].length < numOfLeftColumns - j) {
+            if (leftNum[i].length < numOfLeftColumns - j) {
                 elements.push(<td className="leftNumbers nonogramDefinition">.</td>);
             } else {
                 elements.push(
                     <td className="leftNumbers nonogramDefinition">
-                        {outerLeftNumbers[i][j - numOfLeftColumns + outerLeftNumbers[i].length]}
+                        {leftNum[i][j - numOfLeftColumns + leftNum[i].length]}
                     </td>,
                 );
             }
@@ -152,29 +99,31 @@ const Board: React.FC<BoardProps> = (props) => {
         if (i === 0)
             elements.push(
                 <td
-                    colSpan={numOfUpperRows}
-                    rowSpan={numOfLeftColumns}
+                    colSpan={numOfLeftColumns}
+                    rowSpan={numOfUpperRows}
                     className="nonogramDefinition"
-                />,
+                >
+                    Nonogram Name
+                </td>,
             );
 
-        for (let j = 0; j < props.width; j += 1) {
-            if (outerUpperNumbers[j].length >= numOfUpperRows - i) {
+        for (let j = 0; j < width; j += 1) {
+            if (upNum[j].length >= numOfUpperRows - i) {
                 if (j % 5 === 0) {
                     elements.push(
-                        <td className="upperNumbers column5 nonogramDefinition">
-                            {outerUpperNumbers[j][numOfUpperRows - 1 - i]}
+                        <td className="upperNumbers unColumn5 nonogramDefinition">
+                            {upNum[j][numOfUpperRows - 1 - i]}
                         </td>,
                     );
                 } else {
                     elements.push(
                         <td className="upperNumbers nonogramDefinition">
-                            {outerUpperNumbers[j][numOfUpperRows - 1 - i]}
+                            {upNum[j][numOfUpperRows - 1 - i]}
                         </td>,
                     );
                 }
             } else if (j % 5 === 0) {
-                elements.push(<td className="upperNumbers column5 nonogramDefinition">.</td>);
+                elements.push(<td className="upperNumbers unColumn5 nonogramDefinition">.</td>);
             } else {
                 elements.push(<td className="upperNumbers nonogramDefinition">.</td>);
             }
@@ -186,11 +135,11 @@ const Board: React.FC<BoardProps> = (props) => {
         const elements = [];
 
         /*  otherwise there was thicker line on the left end    */
-        for (let i = 0; i < props.width; i += 1) {
+        for (let i = 0; i < width; i += 1) {
             if (i % 5 === 0) {
-                elements.push(<Tile id={i + j * props.width} col5 />);
+                elements.push(<Tile id={i + j * width} col5 checkCorrect={checkCorrect} />);
             } else {
-                elements.push(<Tile id={i + j * props.width} col5={false} />);
+                elements.push(<Tile id={i + j * width} col5={false} checkCorrect={checkCorrect} />);
             }
         }
         return elements;
@@ -203,7 +152,7 @@ const Board: React.FC<BoardProps> = (props) => {
             elements.push(<tr> {upperRowsGenerator(i)} </tr>);
         }
         /*  we generate whole table */
-        for (let i = 0; i < props.height; i += 1) {
+        for (let i = 0; i < height; i += 1) {
             if (i % 5 === 0) {
                 elements.push(
                     <tr className="row5" key={i.toString()}>
