8f5a58d5085d6d185e44db9994bf8527427abecc
filip12h <martello2311@gmail.com>
2021-05-08 21:19:32 +0200

    updated component Search nonograms
    now you can search for nonograms of particular size, later you filter out those you completed and so on

--- src/components/ListNonograms.tsx
+++ src/components/ListNonograms.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import firebase from '../util/firebase';
 
 type ListNonogramsProps = {
@@ -7,38 +7,85 @@ type ListNonogramsProps = {
 };
 
 const ListNonograms: React.FC<ListNonogramsProps> = ({ showBoard, nonogramList }) => {
+    const [minSize, setMinSize] = useState(0);
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    const [myList, setList] = useState(nonogramList);
+
+    // const sortList = () => {
+    //    nonogramList.sort((a, b) => (a.width * a.height > b.width * b.height ? 1 : -1));
+    // };
+
     /* eslint-disable */
     return (
-        <div className="nonogramList">
-            {nonogramList.map((row) => {
-                let nick =
-                    firebase
-                        .database()
-                        .ref(`User/${row.author}`)
-                        .once('value', (snapshot) => {
-                            return snapshot.val().nickname;
-                        })
-                        .toString() || '?';
-                return (
-                    // IDcko neskor zmazem
-                    // eslint-disable-next-line react/jsx-key
-                    // TODO tu su nejake problemy s databazou
-                    row.enable && (
-                        <div>
-                            <a
-                                role="link"
-                                tabIndex={0}
-                                onClick={() => showBoard(row.id)}
-                                onKeyDown={() => showBoard(row.id)}
-                            >
-                                Author: {row.a}
-                                <br /> {row.width}x{row.height}
-                            </a>
-                        </div>
-                    )
-                );
-            })}
-        </div>
+        <>
+            <label htmlFor="rangeSlider">Minimum size: {minSize}</label>
+            <br />
+            <input
+                type="range"
+                min={0}
+                max={150}
+                multiple
+                value={minSize}
+                onChange={(e) => setMinSize(parseInt(e.target.value))}
+            ></input>
+            <div className="sortContainer">
+                <div>Sort by</div>
+                <div className="sorter">
+                    <span>
+                        <a>date created</a>
+                    </span>
+                    <span>
+                        <a>size</a>
+                    </span>
+                </div>
+                <div className="sorter">
+                    <span>
+                        <a>ascendent</a>
+                    </span>
+                    <span>
+                        <a>descendent</a>
+                    </span>
+                </div>
+                <label htmlFor="single">Singlecolor</label>
+                <input id="single" type="checkbox" />
+                <label htmlFor="multi">Multicolor</label>
+                <input id="multi" type="checkbox" />
+            </div>
+            <br />
+            <div className="nonogramList">
+                {nonogramList.map((row) => {
+                    let nick =
+                        firebase
+                            .database()
+                            .ref(`User/${row.author}`)
+                            .once('value', (snapshot) => {
+                                return snapshot.val().nickname;
+                            })
+                            .toString() || '?';
+                    return (
+                        // IDcko neskor zmazem
+                        // eslint-disable-next-line react/jsx-key
+                        // TODO tu su nejake problemy s databazou
+                        row.enable &&
+                        row.height * row.width > minSize * 10 && (
+                            <div>
+                                <a
+                                    role="link"
+                                    tabIndex={0}
+                                    onClick={() => showBoard(row.id)}
+                                    onKeyDown={() => showBoard(row.id)}
+                                >
+                                    Author: {row.a}
+                                    <br /> {row.width}x{row.height}
+                                    <br />
+                                    colors: {row.usedColors}
+                                </a>
+                            </div>
+                        )
+                    );
+                })}
+            </div>
+        </>
     );
 };
 
